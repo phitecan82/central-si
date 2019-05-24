aec38881d4f1e386f268e7eccc57e9cea234483b
rahmania5 <2574rahmania5@users.noreply.github.com>
2019-05-24 23:18:32 +0700

    Penambahan rekomendasi dan status pada fitur tambah

--- app/Http/Controllers/SemhasController.php
+++ app/Http/Controllers/SemhasController.php
@@ -14,24 +14,36 @@ class SemhasController extends Controller
     {
         $semhass = DB::table('ta_semhas')
                    ->join('ruangan', 'ta_semhas.ruangan_id', '=', 'ruangan.id')
-                   ->select('ta_semhas.id','ta_semhas.semhas_at','ta_semhas.semhas_time','ruangan.nama')
+                   ->join('ta_sempro', 'ta_semhas.ta_sempro_id', '=' , 'ta_sempro.id')
+                   ->join('tugas_akhir','ta_sempro.tugas_akhir_id', '=', 'tugas_akhir.id')
+                   ->join('mahasiswa', 'tugas_akhir.mahasiswa_id', '=', 'mahasiswa.id')
+                   ->select('ta_semhas.id','ta_semhas.semhas_at','ta_semhas.semhas_time','ruangan.nama','ta_sempro.id','mahasiswa.nama as nama_mahasiswa')
                    ->paginate(25);
+        
         return view('backend.semhas.index', compact('semhass'));
     }
 
     public function create()
     {
     	$ruangan = Ruangan::all()->pluck('nama','id');
-        $sempro = DB::table('ta_semhas')
+        $mahasiswa = DB::table('ta_semhas')
                   ->join('ta_sempro', 'ta_semhas.ta_sempro_id', '=' , 'ta_sempro.id')
                   ->join('tugas_akhir','ta_sempro.tugas_akhir_id', '=', 'tugas_akhir.id')
                   ->join('mahasiswa', 'tugas_akhir.mahasiswa_id', '=', 'mahasiswa.id')
                   ->select('ta_sempro.id','mahasiswa.nama')
                   ->pluck('mahasiswa.nama','ta_semhas.id');
-        return view('backend.semhas.create', compact('ruangan','sempro'));
+        $rekomendasi =  DB::table('ta_semhas')
+                    ->select('rekomendasi', DB::raw('(CASE WHEN rekomendasi = 1 THEN '. "'Mengulang Seminar'" .'WHEN rekomendasi = 2 THEN '. "'Lanjut Sidang dengan Revisi'".'WHEN rekomendasi = 3 THEN '."'Lanjut Sidang Tanpa Revisi'".'END) AS rekomendasi_semhas'))
+                    ->distinct()
+                    ->pluck('rekomendasi_semhas','rekomendasi');
+        $status = DB::table('ta_semhas')
+                    ->select('status', DB::raw('(CASE WHEN status = 1 THEN '. "'Sudah terlaksana'" .'WHEN status = 2 THEN '. "'Dalam pengajuan'".'WHEN status = 3 THEN '."'Dibatalkan'".'END) AS status_semhas'))
+                    ->distinct()
+                    ->pluck('status_semhas','status');
+        return view('backend.semhas.create', compact('ruangan','mahasiswa','rekomendasi','status'));
     }
 
-    public  function store(Request $request)
+    public function store(Request $request)
     {
 
     	$request->validate([
@@ -39,6 +51,9 @@ class SemhasController extends Controller
             'semhas_at' => 'required',
             'semhas_time' => 'required',
             'ruangan_id' => 'required',
+            'status' => 'required',
+            'rekomendasi' => 'required',
+            'sidang_deadline_at' => 'required',
     		'file_ba_seminar' => 'file|mimes:pdf',
     		'file_laporan_ta' => 'file|mimes:pdf'
         ]);
